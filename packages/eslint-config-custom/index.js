ab6d2956edbe8b331cbb678d20f080f6cb86f917
Brijesh Bittu <brijeshb42@gmail.com>
2022-12-26 22:36:34 +0530

    Initial commit

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["airbnb", "turbo", "prettier", "plugin:import/typescript"],
+  rules: {
+    "react/jsx-key": "off",
+    "import/prefer-default-export": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": "off",
+    "import/extensions": "off",
+  },
+};
