c937f33473d48cc471ee4310037503e1c5b673be
Brijesh Bittu <brijeshb42@gmail.com>
2022-12-27 21:16:03 +0530

    Build

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [splitVendorChunkPlugin(), react()],
 });
